ca74de50928f164d2f4e0b7681f3e86b4c51cc20
ManarMahmoudAmin <manareltayeb825@gmail.com>
2025-07-18 03:22:56 +0300

    Unit of Work

---
+++ Talabat.Core/Repositories.Contract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Repositories.Contract
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CompleteAsync();
+
+		//Create Repository and return it
+		IGenericRepository<TEntity, TKey> Repository<TEntity,TKey>()
+            where TEntity : BaseEntity<TKey>;
+    }
+}
---
+++ Talabat.Repository/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Repository.Data.Contexts;
+using Talabat.Repository.Repositories;
+
+namespace Talabat.Repository
+{
+	class UnitOfWork : IUnitOfWork
+	{
+		private readonly StoreDbContext _context;
+
+		private readonly Hashtable _repositories;
+
+		public UnitOfWork(StoreDbContext context)
+		{
+			_context = context;
+			_repositories = new Hashtable();
+		}
+		public Task<int> CompleteAsync()
+			=> _context.SaveChangesAsync();
+
+
+		public IGenericRepository<TEntity, TKey> Repository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+		{
+			var repoType = typeof(TEntity).Name;
+
+			//Check if the repository already exists in the hashtable
+			if (!_repositories.ContainsKey((repoType)))
+			{
+				var repository = new GenericRepository<TEntity, TKey>(_context);
+
+				_repositories.Add(repoType, repository);
+			}
+
+			return _repositories[repoType] as IGenericRepository<TEntity, TKey>;
+		}
+	}
+}
