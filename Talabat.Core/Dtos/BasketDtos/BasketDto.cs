5864e92e9e88d47974281dd7af8c735276ab14e0
ManarMahmoudAmin <manareltayeb825@gmail.com>
2025-12-22 03:24:06 +0200

    Validate Customer Basket

--- Talabat.Core/Dtos/BasketDtos/BasketDto.cs
+++ Talabat.Core/Dtos/BasketDtos/BasketDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Talabat.Core.Dtos.BasketDtos
 {
     public class BasketDto
     {
+		[Required]
 		public string Id { get; set; } = default!;
 		public ICollection<BasketItemDto> Items { get; set; } = [];
 	}
