1a43cd94ebf5134048503d962731d3d4982d58e8
ManarMahmoudAmin <manareltayeb825@gmail.com>
2025-12-29 04:38:24 +0200

    Order Module - Entities

---
+++ Talabat.Core/Entities/OrderModule/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.OrderModule
+{
+    public class Order : BaseEntity<int>
+    {
+		public string BuyerEmail { get; set; }
+		public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+		public OrderStatus Status { get; set; } = OrderStatus.Pending;
+		public Address ShippingAddress { get; set; }
+		public DeliveryMethod DeliveryMethod { get; set; }
+		public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+		public decimal SubTotal { get; set; }
+		public string PaymentIntentId { get; set; } = string.Empty;
+		public decimal GetTotal()
+		=> SubTotal + DeliveryMethod.Cost;
+	}
+}
---
+++ Talabat.Core/Entities/OrderModule/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.OrderModule
+{
+    public class OrderItem : BaseEntity<int>
+    {
+		public ProductItemOrdered Product { get; set; }
+		public int Qauntity { get; set; }
+		public decimal Price { get; set; }
+
+	}
+}
